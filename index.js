c9b877dbff85196e6dd88030f5cb9e7e294abab1
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-03-19 00:45:43 +0530

    copy functionality for code snippets

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ var modalBtn = document.querySelector('.modal-btn');
 var modalBg = document.querySelector('.modal-bg');
 var modalClose = document.querySelector('.modal-close');
 
-var copyBtn = document.querySelector('.copy');
-var code = document.getElementById('code');
+//var copyBtn = document.querySelector('.copy');
+var copyBtn = document.querySelectorAll('.copy');
+
+console.log(copyBtn);
+
+//var code = document.getElementById('code');
 var toast = document.querySelector('.copy-toast');
+
 
 modalBtn.addEventListener('click', function() {
     modalBg.classList.add('modal-bg-active');
@@ -28,8 +33,23 @@ for (var i=0; i<btns.length; i++) {
     });
 }
 
-function copyText() {
-    let text = code.innerText;
+// function copyText() {
+//     let text = code.innerText;
+//     navigator.clipboard.writeText(text)
+//     .then(() => {
+//             toast.classList.add('copy-toast-visible');
+//             setTimeout(() => {
+//                 toast.classList.remove('copy-toast-visible');
+//             },2500); 
+//     });
+// }
+
+// copyBtn.addEventListener('click', copyText);
+
+function copyText(str) {
+    console.log(str);
+    let code = document.querySelectorAll(`.${str}`);
+    let text = code[1].innerText;
     navigator.clipboard.writeText(text)
     .then(() => {
             toast.classList.add('copy-toast-visible');
@@ -39,4 +59,4 @@ function copyText() {
     });
 }
 
-copyBtn.addEventListener('click', copyText);
+Array.prototype.map.call(copyBtn, item => item.addEventListener('click', () => copyText(item.classList[2])));
